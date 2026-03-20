5f56f9d5f297f9f165fb56de1e67f77c033db1c0
gwon <arlandzzz4@gmail.com>
2026-03-20 11:57:46 +0900

    S3 및 파일관련 설정 및 소스 추가/Service,ServiceImpl 분리

---
+++ src/main/java/com/project/domain/common/service/impl/S3FileService.java
@@ -0,0 +1,64 @@
+package com.project.domain.common.service.impl;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.project.domain.common.service.FileService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Profile("prod")
+@RequiredArgsConstructor
+public class S3FileService implements FileService {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}") // 2.x 버전은 보통 cloud.aws 로 시작합니다.
+    private String bucket;
+
+    @Override
+    public String upload(MultipartFile file) throws IOException {
+        // 1. 파일명 중복 방지
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        // 2. 메타데이터 설정 (파일 타입 및 크기)
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+
+        // 3. S3 업로드 실행
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, file.getInputStream(), metadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead)); // 외부에서 읽기 가능하도록 설정
+
+        // 4. 저장된 파일의 URL 반환
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    @Override
+    public void delete(String fileName) {
+        // 파일 삭제 (파일명/Key 기준)
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
+    }
+
+	@Override
+	public List<String> uploadList(List<MultipartFile> file) throws IOException {
+		 List<String> fileNames = new java.util.ArrayList<>();
+	     for (MultipartFile multipartFile : file) {
+	         String fileName = upload(multipartFile); // 개별 파일 업로드
+	         fileNames.add(fileName); // 업로드된 파일의 URL을 리스트에 추가
+	     }	
+	     return fileNames;
+	}
+}
