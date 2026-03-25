1f6e4cf5cb0e7531a9350cd51daefa6487005917
gwon <arlandzzz4@gmail.com>
2026-03-25 15:20:09 +0900

    SecurityConfig롤백, 파이어베이스 설정 수정

--- src/main/java/com/project/global/config/FirebaseConfig.java
+++ src/main/java/com/project/global/config/FirebaseConfig.java
@@ -1,33 +1,40 @@
 package com.project.global.config;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 @Profile("prod")
 public class FirebaseConfig {
-	@Value("${FIREBASE_CONFIG_PATH:/app/config/fcm-key.json}")
+	@Value("${FIREBASE_CONFIG_PATH:fcm-key.json}")///app/config/
 	private String configPath;
 	
     @PostConstruct
     public void init() {
         try {
         	if (FirebaseApp.getApps().isEmpty()) {
-	            FileInputStream serviceAccount = new FileInputStream(configPath);
+        		Resource resource = new ClassPathResource(configPath);
+                InputStream serviceAccount = resource.getInputStream();
+                
 	            FirebaseOptions options = FirebaseOptions.builder()
 	                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
 	                    .build();
 	            FirebaseApp.initializeApp(options);
+	            log.info("Firebase Application has been initialized");
         	}
         } catch (IOException e) {
             e.printStackTrace();
