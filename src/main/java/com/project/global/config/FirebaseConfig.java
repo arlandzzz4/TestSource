4d0980efef076707bf1151cd9d533191d7aa28e9
gwon <arlandzzz4@gmail.com>
2026-03-25 16:27:31 +0900

    파이어베이스 수정

--- src/main/java/com/project/global/config/FirebaseConfig.java
+++ src/main/java/com/project/global/config/FirebaseConfig.java
@@ -12,6 +12,7 @@ import org.springframework.core.io.Resource;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.FileSystemResource;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
@@ -23,21 +24,23 @@ public class FirebaseConfig {
 	@Value("${FIREBASE_CONFIG_PATH:fcm-key.json}")///app/config/
 	private String configPath;
 	
-    @PostConstruct
-    public void init() {
-        try {
-        	if (FirebaseApp.getApps().isEmpty()) {
-        		Resource resource = new ClassPathResource(configPath);
-                InputStream serviceAccount = resource.getInputStream();
-                
+	@PostConstruct
+	public void init() {
+	    try {
+	        if (FirebaseApp.getApps().isEmpty()) {
+	            // [변경]: ClassPathResource 대신 FileSystemResource 사용
+	            // 이렇게 해야 도커 볼륨으로 넣은 '/app/config/fcm-key.json'을 읽을 수 있습니다.
+	            Resource resource = new FileSystemResource(configPath); 
+	            InputStream serviceAccount = resource.getInputStream();
+	            
 	            FirebaseOptions options = FirebaseOptions.builder()
 	                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
 	                    .build();
 	            FirebaseApp.initializeApp(options);
-	            log.info("Firebase Application has been initialized");
-        	}
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	            log.info("Firebase 초기화 성공: {}", configPath);
+	        }
+	    } catch (IOException e) {
+	        log.error("Firebase 키 파일을 찾을 수 없습니다: {}", configPath);
+	    }
+	}
 }
