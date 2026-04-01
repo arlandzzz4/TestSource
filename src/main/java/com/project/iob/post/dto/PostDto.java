6f814d2259b83ba87b6de997b6b62c6d0c9973d2
user01837 <aead01837@gmail.com>
2026-04-01 17:27:31 +0900

    s3 api, postwrite api, post api 수정(스웨거에서 테스트를 위해S3Config.java - @Profile({prod, local}) 으로 local 추가됨)

--- src/main/java/com/project/iob/post/dto/PostDto.java
+++ src/main/java/com/project/iob/post/dto/PostDto.java
@@ -3,15 +3,15 @@ package com.project.iob.post.dto;
 import java.sql.Date;
 
 public record PostDto(
-		Long postId, 
-		String userEmail, 
-		String categoryCode, 
-		String title, 
-		Date createdAt, 
-		Date updatedAt, 
-		String nickname, 
-		int likes, 
-		int comments
-		) 
-{
+        Long postId,
+        String userEmail,
+        String categoryCode,
+        String title,
+        Date createdAt,
+        Date updatedAt,
+        String nickname,
+        int likes,
+        int comments,
+        String thumbnail  // 추가
+) {
 }
--- src/main/java/com/project/iob/postwrite/dto/PostWriteDto.java
+++ src/main/java/com/project/iob/postwrite/dto/PostWriteDto.java
@@ -1,10 +1,16 @@
 package com.project.iob.postwrite.dto;
- 
-public record PostWriteDto(
-        String userEmail,
-        String categoryCode,
-        String title,
-        String content
-) {
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostWriteDto {
+    @JsonIgnore  // 요청 시 무시, DB에서 자동생성
+    private Long postId;
+    private String userEmail;
+    private String categoryCode;
+    private String title;
+    private String content;
 }
- 
