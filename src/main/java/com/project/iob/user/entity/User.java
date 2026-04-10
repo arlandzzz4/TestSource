5ecde168aed0fd887b704e9f1afed8dadc182057
gwon <arlandzzz4@gmail.com>
2026-04-10 09:56:10 +0900

    미사용 import제거

--- src/main/java/com/project/iob/user/entity/User.java
+++ src/main/java/com/project/iob/user/entity/User.java
@@ -9,8 +9,6 @@ import com.project.global.enums.Provider;
 import jakarta.annotation.PreDestroy;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PostLoad;
 import jakarta.persistence.PostPersist;
